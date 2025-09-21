369c0462d13d170220c0886ab60301f5dcb01d24
Viktor Gazsi <viktor.gazsi@gmail.com>
2025-09-21 02:29:46 +0200

    Update Astro configuration to enhance image handling and output settings
    - Set output to 'static' in `astro.config.mjs` for optimized deployment.
    - Configured image service with custom entrypoint and disabled input pixel limits for improved image processing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,28 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
-import cloudflare from '@astrojs/cloudflare'
 
 import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://handymanpro.pages.dev',
+  output: 'static',
   integrations: [
     mdx(),
     sitemap(),
     icon({
       iconDir: 'src/assets/icons',
     }),
-    cloudflare(),
   ],
-
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+      config: {
+        limitInputPixels: false,
+      },
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
